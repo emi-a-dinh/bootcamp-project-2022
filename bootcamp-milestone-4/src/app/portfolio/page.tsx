487950f61e88fca9e418055e0b9ce6501537151a
Emi <emidinh6@gmail.com>
2025-01-12 17:19:12 -0800

    feat: fixed vercel errors

--- bootcamp-milestone-4/src/app/portfolio/page.tsx
+++ bootcamp-milestone-4/src/app/portfolio/page.tsx
@@ -9,6 +9,7 @@ export default async function Portfolios({
 }: {
   params: Promise<{ slug: string }>;
 }) {
+  const { slug } = await params;
   const list_of_port = await getPortfolio();
   if (!list_of_port) {
     return <div>Portfolio Not Found</div>;
--- bootcamp-milestone-4/src/database/portfolioSchema.ts
+++ bootcamp-milestone-4/src/database/portfolioSchema.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
   user: { type: String, required: true },
